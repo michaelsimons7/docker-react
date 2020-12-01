9ac66f990774da4acb5fa5b5eae6390189235289
michaelsimons7 <michael.simons7@gmail.com>
2020-12-01 20:49:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to bum bum baby boo. Don't believe it scooby doo!
+          Still not!
+        </p>
+        <p>
+          And how about now?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
