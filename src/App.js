892d2c30a0e21566198ab831807844708fb43afd
michaelsimons7 <michael.simons7@gmail.com>
2020-12-27 15:29:20 +0200

    Text in app.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
         <p>
           And how about now?
         </p>
+        <p>And what about this, eh?</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
